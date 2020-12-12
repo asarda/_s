922647af93e578214aa2dc404f8750a2b577f99e
Albert Sardà <asarda82@gmail.com>
2020-12-12 12:56:51 +0100

    wrapp page and post into bulma columns

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 WPkickoff-bulma
 ===
 
-This is a WordPress starter teme based on the _s starter theme by Automattic (http://underscores.me/)
-
+WPkickoff-bulma is a WordPress starter teme. It based on Bulma Framework and on the _s starter theme by Automattic (http://underscores.me/). You can use it as starter theme itself and build your own design theme.
 Includes a Webpack configuration for managing and bundling front end assets.
--- page.php
+++ page.php
@@ -14,25 +14,28 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="columns">
+		<div class="column is-three-quarters">
+			<main id="primary" class="site-main">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-	<main id="primary" class="site-main">
+					get_template_part( 'template-parts/content', 'page' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+				endwhile; // End of the loop.
+				?>
+			</main><!-- #main -->
+		</div>
+		<div class="column">
+			<?php get_sidebar(); ?>
+		</div>	
+	</div>
+</div>
+<?php get_footer(); ?>
